97dd3995ac5f8929c4eebffe17c3ff57b889d192
Benedikt Geißler <benedikt@g5r.eu>
2016-11-06 17:31:31 +0100

    Indent HTML with 2 spaces and outsource JS and CSS

---
+++ tut2/script.js
@@ -0,0 +1,11 @@
+var lastField = null;
+var currentFillColor = '';   // ???
+var changeCounter = 0;       // ???
+
+function setField(element) {
+    // element contains the current html element
+}
+
+function setFillColor(color) {
+    // color should be a string
+}
