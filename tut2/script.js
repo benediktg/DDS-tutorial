c85396953af0b1bc7b71184a39bf9453eed84ea6
Benedikt Geißler <benedikt@g5r.eu>
2016-11-06 19:05:14 +0100

    Implement tut2 step 2
    If the users clicks on a field in the upper grey square,
    the color of the clicked field should be changed
     to the previously selected color.

--- tut2/script.js
+++ tut2/script.js
@@ -4,6 +4,7 @@ var changeCounter = 0;       // ???
 
 function setField(element) {
     // element contains the current html element
+    element.style.backgroundColor = currentFillColor;
 }
 
 function setFillColor(color) {
