e731dfc1f61472243a66553d45bfad938a96389c
Benedikt Geißler <benedikt@g5r.eu>
2016-11-09 03:13:08 +0100

    Add semicolons

--- tut3/script.js
+++ tut3/script.js
@@ -1,36 +1,40 @@
-var text = ""
-var fieldX = ""
-var fieldY = ""
-var css = ""
-var table = document.getElementsByTagName("tbody").item(0)
+var text = "";
+var fieldX = "";
+var fieldY = "";
+var css = "";
+var table = document.getElementsByTagName("tbody")[0];
 
 function setTableElement(i, j, text, css) {
-    table.children[i].children[j].innerHTML = text
-    table.children[i].children[j].style = css
+    table.children[i].children[j].innerHTML = text;
+    table.children[i].children[j].style = css;
+}
+
+function getTableElement(i, j) {
+    return table.children[i].children[j];
 }
 
 function handleTextInput() {
-    text = document.getElementById("text").value
+    text = document.getElementById("text").value;
 }
 
 function handleFieldXInput() {
-    fieldX = document.getElementById("field_x").value
+    fieldX = document.getElementById("field_x").value;
 }
 
 function handleFieldYInput() {
-    fieldY = document.getElementById("field_y").value
+    fieldY = document.getElementById("field_y").value;
 }
 
 function handleCssInput() {
-    css = document.getElementById("css").value
+    css = document.getElementById("css").value;
 }
 
 function handleMyButton() {
 
 }
 
-document.getElementById("text").addEventListener("change", handleTextInput)
-document.getElementById("field_x").addEventListener("change", handleFieldXInput)
-document.getElementById("field_y").addEventListener("change", handleFieldYInput)
-document.getElementById("css").addEventListener("change", handleCssInput)
-document.getElementById("mybutton").addEventListener("click", handleMyButton)
+document.getElementById("text").addEventListener("change", handleTextInput);
+document.getElementById("field_x").addEventListener("change", handleFieldXInput);
+document.getElementById("field_y").addEventListener("change", handleFieldYInput);
+document.getElementById("css").addEventListener("change", handleCssInput);
+document.getElementById("mybutton").addEventListener("click", handleMyButton);
