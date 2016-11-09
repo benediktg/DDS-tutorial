6a40de48106c093c491836a255c7927028ccd893
Benedikt Geißler <benedikt@g5r.eu>
2016-11-09 02:53:35 +0100

    Begin with homework 3

--- tut3/script.js
+++ tut3/script.js
@@ -1,1 +1,36 @@
+var text = ""
+var fieldX = ""
+var fieldY = ""
+var css = ""
+var table = document.getElementsByTagName("tbody").item(0)
 
+function setTableElement(i, j, text, css) {
+    table.children[i].children[j].innerHTML = text
+    table.children[i].children[j].style = css
+}
+
+function handleTextInput() {
+    text = document.getElementById("text").value
+}
+
+function handleFieldXInput() {
+    fieldX = document.getElementById("field_x").value
+}
+
+function handleFieldYInput() {
+    fieldY = document.getElementById("field_y").value
+}
+
+function handleCssInput() {
+    css = document.getElementById("css").value
+}
+
+function handleMyButton() {
+
+}
+
+document.getElementById("text").addEventListener("change", handleTextInput)
+document.getElementById("field_x").addEventListener("change", handleFieldXInput)
+document.getElementById("field_y").addEventListener("change", handleFieldYInput)
+document.getElementById("css").addEventListener("change", handleCssInput)
+document.getElementById("mybutton").addEventListener("click", handleMyButton)
