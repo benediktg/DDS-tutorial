1a59e22ff47fec564a16264d96fc18314c936308
Benedikt Geißler <benedikt@g5r.eu>
2016-11-15 02:35:59 +0100

    Implement deleting entries

--- tut4/script.js
+++ tut4/script.js
@@ -1,5 +1,6 @@
 var list = document.getElementsByTagName("ul")[0];
 var url = "https://vsr.informatik.tu-chemnitz.de/edu/2015/evs/exercises/jsajax/guestbook.php";
+var localEntries = {};
 list.removeChild(list.firstElementChild);
 list.removeChild(list.firstElementChild);
 loadList();
@@ -18,9 +19,13 @@ function loadList() {
 
 function createEntries(array) {
     "use strict";
+    var entryCount = array.length;
     var i;
     var entry;
-    for (i = 0; i < array.length; ++i) {
+    for (i = 0; i < entryCount; ++i) {
+        if (String(array[i].id) in localEntries) {
+            continue;
+        }
         entry = document.createElement("li");
         entry.innerHTML = "<b>" + array[i].name + ":</b> " + array[i].text
             + " <a href=\"#\" alt=\"Delete entry\">(X)</a>";
@@ -29,10 +34,16 @@ function createEntries(array) {
             removeEntry(this);
         });
         list.appendChild(entry);
+        localEntries[String(array[i].id)] = entry;
     }
 }
 
 function removeEntry(aTag) {
     "use strict";
     var id = aTag.parentElement.getAttribute("entry-id");
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", url + "?id=" + id, true);
+    xhr.send();
+    list.removeChild(localEntries[id]);
+    delete localEntries[id];
 }
