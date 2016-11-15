9c30318dfd31a26f3347f3aa8261b9e87e3eaf5b
Benedikt Geißler <benedikt@g5r.eu>
2016-11-15 18:03:14 +0100

    Posting entries works, but not as expected

--- tut4/script.js
+++ tut4/script.js
@@ -1,8 +1,10 @@
 var list = document.getElementsByTagName("ul")[0];
+var form = document.forms[0];
 var url = "https://vsr.informatik.tu-chemnitz.de/edu/2015/evs/exercises/jsajax/guestbook.php";
 var localEntries = {};
 list.removeChild(list.firstElementChild);
 list.removeChild(list.firstElementChild);
+form.getElementsByTagName("button")[0].addEventListener("click", postEntry)
 loadList();
 
 function loadList() {
@@ -15,6 +17,7 @@ function loadList() {
     };
     xhr.open("GET", url, true);
     xhr.send();
+    return;
 }
 
 function getEntries(array) {
@@ -36,6 +39,7 @@ function getEntries(array) {
         list.appendChild(entry);
         localEntries[String(array[i].id)] = entry;
     }
+    return;
 }
 
 function removeEntry(aTag) {
@@ -46,8 +50,38 @@ function removeEntry(aTag) {
     xhr.send();
     list.removeChild(localEntries[id]);
     delete localEntries[id];
+    return;
 }
 
 function postEntry() {
+    "use strict";
+    var xhr = new XMLHttpRequest();
+    var name = form["name"];
+    var text = form["text"];
+    var response;
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            response = JSON.parse(this.responseText);
+            getSingleEntry(response.entry);
+        }
+    };
+    xhr.open("POST", url, true);
+    xhr.send("name=" + name + "&text=" + text);
+    return;
+}
 
+function getSingleEntry(object) {
+    if (String(object.id) in localEntries) {
+        return;
+    }
+    entry = document.createElement("li");
+    entry.innerHTML = "<b>" + object.name + ":</b> " + object.text
+        + " <a href=\"#\" alt=\"Delete entry\">(X)</a>";
+    entry.setAttribute("entry-id", object.id);
+    entry.lastElementChild.addEventListener("click", function () {
+        removeEntry(this);
+    });
+    list.appendChild(entry);
+    localEntries[String(object.id)] = entry;
+    return;
 }
