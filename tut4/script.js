20e4028d312b4a2f9c4de0443faa9e6d502959c2
Benedikt Geißler <benedikt@g5r.eu>
2016-11-15 01:18:01 +0100

    Show the guestbook entries

--- tut4/script.js
+++ tut4/script.js
@@ -1,1 +1,29 @@
+var list = document.getElementsByTagName("ul")[0];
+list.removeChild(list.firstElementChild);
+list.removeChild(list.firstElementChild);
+loadList();
 
+function loadList() {
+    "use strict";
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            createEntries(JSON.parse(this.responseText));
+        }
+    };
+    var url = "https://vsr.informatik.tu-chemnitz.de/edu/2015/evs/exercises/jsajax/guestbook.php";
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+function createEntries(array) {
+    "use strict";
+    var i;
+    var entry;
+    for (i = 0; i < array.length; ++i) {
+        entry = document.createElement("li");
+        entry.innerHTML = "<b>" + array[i].name + ":</b> " + array[i].text
+            + " <a href=\"#\" alt=\"Delete entry\">(X)</a>";
+        list.appendChild(entry);
+    }
+}
