a005ed629f02f7f61ac2719ad7286693935fbfab
Benedikt Geißler <benedikt@g5r.eu>
2016-11-23 00:08:08 +0100

    Implement adding rows to the table

--- tut5/script.js
+++ tut5/script.js
@@ -1,3 +1,20 @@
-$(document).ready(function () {
+$(document).ready(function main() {
+    $("#add").click(function addRow() {
+        var name = $("<td></td>").text($("#name").val());
+        var up = $("<button></button>").text("Move up").click(moveUp);
+        var down = $("<button></button>").text("Move down").click(moveDown);
+        var operations = $("<td></td>").append(up, down);
+        var row = $("<tr></tr>").append(name, operations);
+        $("#nameList").append(row);
+        $("#name").val("");
+    })
 
 })
+
+function moveUp() {
+
+}
+
+function moveDown() {
+
+}
