31f7ba378bb33611faca22be06c86985bedb3cb5
Benedikt GeiÃler <benedikt@g5r.eu>
2016-12-05 21:33:07 +0100

    Style correction

--- tut2/script.js
+++ tut2/script.js
@@ -1,25 +1,24 @@
-var lastField = null;
-var currentFillColor = '';   // ???
-var changeCounter = 0;       // ???
+var currentFillColor = "";
+var changeCounter = 0;
 
 function setField(element) {
     // element contains the current html element
     if (element.style.backgroundColor !== currentFillColor) {
         element.style.backgroundColor = currentFillColor;
     } else {
-        element.style.backgroundColor = '#eee';
+        element.style.backgroundColor = "#eee";
     }
     ++changeCounter;
     if (changeCounter === 10) {
-        alert('You have clicked 10 times on the field!');
+        alert("You have clicked 10 times on the field!");
         changeCounter = 0;
     }
 }
 
 function setFillColor(color) {
     // color should be a string
-    var logEntry = document.createElement('p');
-    logEntry.innerHTML = 'Color changed';
+    var logEntry = document.createElement("p");
+    logEntry.innerHTML = "Color changed";
     logEntry.style.color = currentFillColor = color;
-    document.getElementById('Log').appendChild(logEntry);
+    document.getElementById("Log").appendChild(logEntry);
 }
